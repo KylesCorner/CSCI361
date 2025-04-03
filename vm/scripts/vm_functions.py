fe6f29887873b8f59fb56be2113358da8a11f4a7
KylesCorner <kylekrstulich@gmail.com>
2025-04-03 10:12:39 -0600

    Documentation

--- vm/scripts/vm_functions.py
+++ vm/scripts/vm_functions.py
@@ -5,17 +5,62 @@ vm_functions.py
 
 Virtual machine functions for the nand to tetris project.
 
-Assignment details:
+Project 6 Assignment details:
 
-    Write functions that take no arguments and return strings of Hack machine
-    language to achieve a pop of the stack to the D register and a push of the D
-    register onto the stack. Assume a symbol "SP" which is the memory address of the
-    stack pointer exists. Call these functions 'getPushD()' and 'getPopD()'
-    respectively. Don't forget to keep the position of the stack pointer up-to-date
+    Write functions that take no arguments and return strings of Hack machine language to achieve a
+    pop of the stack to the D register and a push of the D register onto the stack. Assume a symbol
+    "SP" which is the memory address of the stack pointer exists. Call these functions 'getPushD()'
+    and 'getPopD()' respectively. Don't forget to keep the position of the stack pointer up-to-date
     after the push or pop is done.
 
-    Instead of placing new line characters in your strings, just use commas (,) to
-    separate the lines.
+    Instead of placing new line characters in your strings, just use commas (,) to separate the
+    lines.
+
+Project 7 Assignment details:
+
+    To accomplish our objectives, we will work with three different types of segments for VM
+    language constructs like:
+
+    push SEGMENT INDEX or,
+
+    pop SEGMENT INDEX if SEGMENT is 'local', 'argument', 'this', or 'that':
+
+    those are pointers, meaning the RAM address associated with them in the symbol table contains a
+    RAM address where the SEGMENT begins. INDEX specifies where to go in the SEGMENT.
+
+    if SEGMENT is 'constant' or 'static':
+
+    Then something special occurs, in this case it is more akin to direct manipulation of the value
+    in INDEX onto or off of the stack.
+
+    if SEGMENT is 'pointer' or 'temp':
+
+    Here, the values in the memory address specified by the segment are defined, 3 and 5
+    respectively and not indirectly specified pointers.
+
+    So, for each of the above there is a different function. For our next class, do the first set of
+    segments for local, argument, this, and that. The function follows. Implement it.
+
+    def pointerSeg(pushpop,seg,index):
+        This function returns Hack ML code to push a memory location to 
+        to the stack, or pop the stack to a memory location. 
+
+        INPUTS:
+            pushpop = a text string 'pop' means pop to memory location, 'push' 
+                      is push memory location onto stack
+            seg     = the name of the segment that will be be the base address
+                      in the form of a text string
+            index   = an integer that specifies the offset from the base 
+                      address specified by seg
+
+        RETURN: 
+            The memory address is speficied by segment's pointer (SEGLABEL[seg]) + index (index))
+            if pushpop is 'push', push the address contents to the stack
+            if pushpop is 'pop' then pop the stack to the memory address.
+            The string returned accomplishes this. ML commands are seperated by commas (,).
+
+        NOTE: This function will only be called if the seg is one of:
+        "local", "argument", "this", "that"
 """
 def getPopD():
     return "@SP,AM=M-1,D=M,"
@@ -24,7 +69,28 @@ def getPushD():
     return "@SP,A=M,M=D,@SP,M=M+1",
 
 def pointerSeg(pushpop, seg, index):
-    """Generate Hack assembly code for push/pop operations on pointer-based segments."""
+    """
+    Generate Hack assembly code for push/pop operations on pointer-based segments.
+
+
+    INPUTS:
+        pushpop = a text string 'pop' means pop to memory location, 'push' 
+                  is push memory location onto stack
+        seg     = the name of the segment that will be be the base address
+                  in the form of a text string
+        index   = an integer that specifies the offset from the base 
+                  address specified by seg
+
+    RETURN: 
+        The memory address is speficied by segment's pointer (SEGLABEL[seg]) + index (index))
+        if pushpop is 'push', push the address contents to the stack
+        if pushpop is 'pop' then pop the stack to the memory address.
+        The string returned accomplishes this. ML commands are seperated by commas (,).
+
+    NOTE: This function will only be called if the seg is one of:
+    "local", "argument", "this", "that"
+
+    """
     # Base addresses for segments in Hack assembly
     SEGLABEL = {
         "local": "LCL",
